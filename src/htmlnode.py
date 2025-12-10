9ff3278ed21dfbd210804f4de21e1422a8b152e4
bruttins <bruttins@gmail.com>
2025-12-10 18:14:55 +0100

    leafnode added, started parentnode

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -20,3 +20,32 @@ class HTMLNode:
 
     def __repr__(self):
         return f"HTMLNode({self.tag}, {self.value}, {self.children}, {self.props})"
+
+
+class LeafNode(HTMLNode):
+    def __init__(self, tag, value, props=None):
+        super().__init__(tag=tag, value=value, children=None, props=props)
+
+    def to_html(self):
+        if self.value is None:
+            raise ValueError("Value missing")
+        elif self.tag is None:
+            return f"{self.value}"
+        else:
+            adress = self.props_to_html()
+            return f"<{self.tag}{adress}>{self.value}</{self.tag}>"
+
+class ParentNode(HTMLNode):
+    def __init__(self, tag, children, props=None):
+        super().__init__(tag=tag, value=None, children=children, props=props)
+
+    def to_html(self):
+        if self.tag is None:
+            raise ValueError("Tag missing")
+        elif self.children is None:
+            raise ValueError("Children missing, call 911")
+        else:
+            childrenlist = []
+            for child in self.children:
+                childrenlist.append(#recursive to_html-call)
+            return childrenlist
--- src/test_htmlnode.py
+++ src/test_htmlnode.py
@@ -1,20 +1,42 @@
 import unittest
-from htmlnode import HTMLNode
+from htmlnode import HTMLNode, LeafNode
 
 class TestHTMLNode(unittest.TestCase):
     def test_true(self):
         node = HTMLNode("a", "click here", None, {"href": "www.clicker.com"})
-        assert node.props_to_html() == ' href="www.clicker.com"'
+        self.assertEqual(node.props_to_html(), ' href="www.clicker.com"')
 
     def test_propsmissing(self):
         node = HTMLNode("a", "click here", None, None)
-        assert node.props_to_html() == ""
+        self.assertEqual(node.props_to_html(), "")
 
     def test_multipleprops(self):
         node = HTMLNode("a", "click here", None, {"href": "www.clicker.com", "target": "_landingpage"})
         props_html = node.props_to_html()
-        assert ' href="www.clicker.com"' in props_html
-        assert ' target="_landingpage"' in props_html
+        self.assertIn(' href="www.clicker.com"', props_html)
+        self.assertIn(' target="_landingpage"', props_html)
+
+class TestLeafNode(unittest.TestCase):
+    def test_leaf_to_html_p(self):
+        node = LeafNode("p", "Hello, world!")
+        self.assertEqual(node.to_html(), "<p>Hello, world!</p>")
+
+    def test_leaf_to_html_b(self):
+        node = LeafNode("b", "This is fat.")
+        self.assertEqual(node.to_html(), "<b>This is fat.</b>")
+
+    def test_leaf_to_html_a(self):
+        node = LeafNode("a", "Click me", {"href": "www.whatalink.com"})
+        self.assertEqual(node.to_html(), '<a href="www.whatalink.com">Click me</a>')
+
+    def test_noTag(self):
+        node = LeafNode(None, "Some Text", {"href": "www.linky.com"})
+        self.assertEqual(node.to_html(), "Some Text")
+
+    def test_noValue(self):
+        node = LeafNode("b", None)
+        with self.assertRaises(ValueError):
+            node.to_html()
 
 if __name__ == "__main__":
     unittest.main()
